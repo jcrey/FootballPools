a79efb01be7038fb9995b1e603a9f123a4d6ca69
Carlos Reyes <jcreyes@sciodev.com>
2015-06-25 12:33:57 -0500

    Add project

---
+++ FootballPools/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FootballPools.MvcApplication" Language="C#" %>
---
+++ FootballPools/Utils/UserIdentityExtensions.cs
@@ -0,0 +1,18 @@
+﻿using FootballPools.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FootballPools.Utils
+{
+    public static class UserIdentityExtensions
+    {
+        private static CrmUserRepository allUsers = new CrmUserRepository();
+        public static Guid GetUserId(this System.Security.Principal.IIdentity identity)
+        {
+            var usr = allUsers.GetSingle(where: new { UserName = identity.Name });
+            return usr!= null? usr.Id : Guid.Empty;
+        }
+    }
+}
